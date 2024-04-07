1dffc290c75f0ceca6cc13bc608e3389cf8a31d0
justinkuruvila54 <justinkuruvila54@gmail.com>
2024-04-07 18:43:27 +0530

    firebaseinitail

--- android/app/src/main/kotlin/com/total_x/total_x_tes/MainActivity.kt
+++ android/app/src/main/kotlin/com/total_x/total_x_tes/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.total_x.total_x_test
+package com.total_x.total_x_tes
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ import 'package:total_x_test/view/loginpage/loginscreen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyBRmqXUSUMcqpVgiW08FbP17iYTuF3i6Sc",
+          appId: "1:365317921352:android:de325b891023f4d692167d",
+          messagingSenderId: "",
+          projectId: "firebasadatabaeapp",
+          storageBucket: "firebasadatabaeapp.appspot.com"));
   runApp(Myapp());
 }
 
